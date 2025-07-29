9f2758aa6a2a82cef9733a83f5c0eaa601b088fe
zszf114514 <denys2010@foxmail.com>
2025-07-29 20:57:51 +0800

    chore: use symlink::symlink_auto

--- src/utils.rs
+++ src/utils.rs
@@ -2,29 +2,9 @@ pub(crate) mod hashing;
 
 use std::io;
 use std::path::Path;
-use tokio::fs;
 use symlink;
 
-#[cfg(unix)]
-pub fn symlink_dir<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
-    std::os::unix::fs::symlink(from, to)?;
-    Ok(())
-}
-
-#[cfg(windows)]
-pub fn symlink_dir<P:AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
-    symlink::symlink_dir(from, to)?;
-    Ok(())
-}
-
-#[cfg(unix)]
-pub fn symlink_file<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
-    std::os::unix::fs::symlink(from, to)?;
-    Ok(())
-}
-
-#[cfg(windows)]
-pub fn symlink_file<P:AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
-    symlink::symlink_file(from, to)?;
+pub fn create_symlink<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> io::Result<()>{
+    symlink::symlink_auto(from, to)?;
     Ok(())
 }
